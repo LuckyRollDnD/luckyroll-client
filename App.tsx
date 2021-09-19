aea3bf72d7ef8cd5723ceb1335ca38d88162cba8
bryan leighton <bryan.leigh77@gmail.com>
2021-09-18 23:40:09 -0700

    Basic React Native setup for Ios

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import HomeScreen from './src/screens/HomeScreen';
+import Register from './src/screens/Register';
+
+const cache = new InMemoryCache()
+
+const url = 'http://localhost:4000/graphql'
+const client = new ApolloClient({
+  uri: url,
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+
+});
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </ApolloProvider>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/screens/Register.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { Text } from "react-native";
+interface Props {
+
+}
+
+interface FormValues {
+    email: string;
+    password: string;
+    confirmPassword: string
+}
+
+export default function Register(){
+
+    const []
+    return <Text>Register Page</Text>;
+}
